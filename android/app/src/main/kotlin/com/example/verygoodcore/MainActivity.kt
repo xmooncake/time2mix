eed907721cc5dd772b31077e08b7fd5861696986
s.gierlach@pralnia77.pl <128915184+xmooncake@users.noreply.github.com>
2024-03-16 12:27:35 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.time2mix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:time2mix/app/app.dart';
+import 'package:time2mix/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
